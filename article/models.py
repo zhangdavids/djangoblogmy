706a47dfee325eeee2bad6df82cfe237733adeec
zhangdavids <1269969739@qq.com>
2016-04-23 16:25:12 +0800

    duoshuo,markdown and code highlight

--- article/models.py
+++ article/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 # Create your models here.
 
@@ -8,7 +9,13 @@ class Article(models.Model):
     date_time=models.DateTimeField(auto_now_add=True)
     content=models.TextField(blank=True,null=True)
 
-    def __unicode__(self):
+
+    def get_absolute_url(self):
+        path = reverse('detail', kwargs={'id':self.id})
+        return "http://127.0.0.1:8089%s" % path
+
+
+    def __str__(self):
         return self.title
 
     class Meta:
