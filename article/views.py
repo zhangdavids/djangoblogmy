557287667807bdf99d7884545c8aba22f6c67589
zhangdavids <1269969739@qq.com>
2016-04-23 12:47:07 +0800

    django tutorial init

---
+++ article/views.py
@@ -0,0 +1,19 @@
+# _*_ coding: utf-8 _*_
+
+from django.shortcuts import render
+from django.http import HttpResponse
+from article.models import Article
+from datetime import datetime
+
+# Create your views here.
+
+def home(request):
+    post_list=Article.objects.all()
+    return render(request,'home.html',{'post_list':post_list})
+
+
+
+
+
+
+
