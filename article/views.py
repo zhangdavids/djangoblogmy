48e1f7c4b31626b816dd7f459afc9c45e45fe8ee
zhangdavids <1269969739@qq.com>
2016-04-23 21:27:19 +0800

    add tag

--- article/views.py
+++ article/views.py
@@ -19,6 +19,26 @@ def detail(request, id):
         raise Http404
     return render(request, 'post.html', {'post' : post})
 
+#归档
+def archives(request) :
+    try:
+        post_list = Article.objects.all()
+    except Article.DoesNotExist :
+        raise Http404
+    return render(request, 'archives.html', {'post_list' : post_list,
+                  'error' : False})
+#about me
+def about_me(request) :
+    return render(request, 'aboutme.html')
+
+#search,标签分类
+def search_tag(request, tag) :
+    try:
+        post_list = Article.objects.filter(category__iexact = tag) #contains
+    except Article.DoesNotExist :
+        raise Http404
+    return render(request, 'tag.html', {'post_list' : post_list})
+
 
 
 
--- my_blog/urls.py
+++ my_blog/urls.py
@@ -23,4 +23,7 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$','article.views.home'),
     url(r'^(?P<id>\d+)/$', 'article.views.detail', name='detail'),
+    url(r'^archives/$', 'article.views.archives', name = 'archives'),
+    url(r'^aboutme/$', 'article.views.about_me', name = 'about_me'),
+    url(r'^tag(?P<tag>\w+)/$', 'article.views.search_tag', name = 'search_tag'),
     )
