102acef86b512735213cd5d6073b7003e535c38a
zhangdavids <1269969739@qq.com>
2016-04-23 14:17:55 +0800

    dynamic url

--- my_blog/urls.py
+++ my_blog/urls.py
@@ -22,4 +22,5 @@ urlpatterns = patterns('',
                        
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$','article.views.home'),
+    url(r'^(?P<id>\d+)/$', 'article.views.detail', name='detail'),
     )
